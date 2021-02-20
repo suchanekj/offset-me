95a9d79d448613bee6bcf6060a6a7e2303975166
Lawrence Berry <lawrenceberry@outlook.com>
2021-02-20 17:00:01 +0000

    Source data from filesystem

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: "offset-me",
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-csv`,
+      options: {
+        ignoreEmpty: true,
+        nodePerFile: true,
+      },
+    },
+  ],
 };
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,8 +1,55 @@
 import React, { useState } from "react";
 import "../styles.scss";
 import { Layout } from "../components";
+import { useStaticQuery } from "gatsby";
 
 export default function Index() {
+  const data = useStaticQuery(graphql`
+    query {
+      allCalculationsCsv {
+        nodes {
+          items {
+            Tier_1
+            Tier_2
+            Tier_3
+            Low
+            Mid
+            High
+            Max
+            Unit
+            Malaria_Consortium
+            Against_Malaria_Foundation
+            Deworm_the_World
+            Helen_Keller_International
+            New_Incentives
+            GiveDirectly
+            The_END_Fund
+            SCI_Foundation
+            Sightsavers
+            Village_Enterprise
+            Seva
+            JAAGO_Foundation
+            UNICEF_south_africa
+            Lola_Karimova_Tillyaeva
+            Cambodian_Children_s_Fund
+            Plan_vivo
+            Gold_Standard
+            Founders_Pledge_Climate_change_fund
+            Clean_Air_Task_Force
+            WILD_Foundation
+            Cool_earth
+            ACE_s_Recommended_Charity_Fund
+            New_Harvest
+            Oceana
+            Environmental_Working_Group
+            Rainforest_alliance
+            Sustainable_Food_Trust
+          }
+        }
+      }
+    }
+  `);
+
   const [value, setValue] = useState(0);
 
   const handleChange = (e) => {
