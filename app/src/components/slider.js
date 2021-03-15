07895a63242c22d79614bacb4d9f88eb83f82ff8
Jakub Suchanek <suchanek989@seznam.cz>
2021-03-15 11:11:03 +0100

    add initial values and slider ticks

--- app/src/components/slider.js
+++ app/src/components/slider.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Slider = ({ name, value, setValue, max, unit, level }) => {
+export const Slider = ({ name, value, setValue, max, unit, level, high, low, mid }) => {
   const handleChange = (e) => {
     setValue(Number(e.target.value));
   };
@@ -9,16 +9,23 @@ export const Slider = ({ name, value, setValue, max, unit, level }) => {
       <label for="slider" class={"column form-text form-level-" + String(level)}>
         {name}
       </label>
-      <input
-        id="slider"
-        class="column slider is-circle"
-        type="range"
-        step="0.01"
-        min={0}
-        max={max}
-        value={value}
-        onChange={handleChange}
-      />
+      <div class="slider-container">
+        <input
+          id="slider"
+          className="column slider is-circle"
+          type="range"
+          step="0.01"
+          min={0}
+          max={max}
+          value={value}
+          onChange={handleChange}
+        />
+        <div className="slider-marks">
+          <span className="slider-mark" style={{marginLeft: String(Number(low) / Number(max) * 100) + "%"}}>▶</span>
+          <span className="slider-mark" style={{marginLeft: String((Number(mid) - Number(low)) / Number(max) * 100) + "%"}}>▲</span>
+          <span className="slider-mark" style={{marginLeft: String((Number(high) - Number(mid)) / Number(max) * 100) + "%", marginRight: "auto"}}>◀</span>
+        </div>
+      </div>
       <output for="slider" class="column form-value">
         {unit === "%" ? `${value.toFixed(1)} %` : `£ ${value}`}
       </output>
