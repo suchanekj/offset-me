85790c1f0439f3eb0eebafadf6eea6bd1a459707
Lawrence Berry <lawrenceberry@outlook.com>
2021-04-07 16:33:10 +0100

    Add in PayPal's commission

--- app/src/components/paypal.js
+++ app/src/components/paypal.js
@@ -4,15 +4,22 @@ import Paypal from "gatsby-plugin-paypal";
 export const PayPalButton = ({ items }) => {
   const total = items.reduce((acc, [_, value]) => acc + value, 0);
 
+  // Add in PayPal's commission
+  // https://www.paypal.com/uk/webapps/mpp/merchant-fees
+  const commission = 0.3 + 0.012 * total;
+
+  const totalWithCommission = total + commission;
+
   const createOrderPaypal = (data, actions) => {
     return actions.order.create({
       purchase_units: [
         {
           amount: {
             currency_code: "GBP",
-            value: total.toFixed(2),
+            value: totalWithCommission.toFixed(2),
             breakdown: {
               item_total: { currency_code: "GBP", value: total.toFixed(2) },
+              handling: { currency_code: "GBP", value: commission.toFixed(2) },
             },
           },
           items: items.map(([name, value]) => ({
@@ -35,6 +42,22 @@ export const PayPalButton = ({ items }) => {
         }}
         createOrder={createOrderPaypal}
       />
+      <div class="card">
+        <div class="card-content has-text-left">
+          <div class="columns">
+            <div class="column is-italic">Donation:</div>
+            <div class="column">£{total.toFixed(2)}</div>
+          </div>
+          <div class="columns">
+            <div class="column is-italic">PayPal processing fee:</div>
+            <div class="column">£{commission.toFixed(2)}</div>
+          </div>
+          <div class="columns">
+            <div class="column has-text-weight-semibold">Total:</div>
+            <div class="column">£{totalWithCommission.toFixed(2)}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
