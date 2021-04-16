dc6b293c6152534913218b620b893baf0b880521
Jakub Suchanek <suchanek989@seznam.cz>
2021-04-16 10:15:54 +0200

    fix documentation math rendering

--- app/src/pages/documentation.js
+++ app/src/pages/documentation.js
@@ -5,9 +5,8 @@ export default function Documentation() {
   return (
     <Layout>
       <div className="box">
-        {/*<h1>A place to show statistics about donations.</h1>*/}
-        <script src="https://offset-me.suchanek.io/docs/resize.js" type="text/javascript" />
-        <iframe width="100%" height="11000px" src="https://offset-me.suchanek.io/docs" title="W3Schools Free Online Web Tutorials" onload="resizeIframe(this)"></iframe>
+        {/*<script src="../docs/resize.js" type="text/javascript" />*/}
+        <iframe width="100%" height="12000px" src="https://offset-me.suchanek.io/docs"></iframe>
       </div>
     </Layout>
   );
